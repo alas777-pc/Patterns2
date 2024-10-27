dd8695850ff108e248ce02c43cce080b1df8337a
Alexei Lubinets <lubinets01@mail.ru>
2024-10-28 00:32:09 +0500

    initial commit

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,33 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataGenerator {
+    private static final Faker FAKER = new Faker (new Locale("en"));
+    private DataGenerator() {
+
+    }
+    public static String getRandomLogin() {return FAKER.name().username();};
+    public static String getRandomPassword() {return FAKER.internet().password();};
+
+    public static class Registration {
+        private Registration() {
+
+        }
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+        }
+        public static RegistrationDto getRegisteredUser(String status) {
+            return ApiHelper.sendRequest(getUser(status));
+        }
+    }
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
